2df17c7efdb8f3306c15a7883ae7c4fe05d4f31c
Aharoni <you@example.com>
2017-08-10 10:50:25 +0300

    add navigation bar

--- app/shared/navCtrl.js
+++ app/shared/navCtrl.js
@@ -1,6 +1,14 @@
-coachApp.controller("NavCtrl", function ($scope, activeUser) { 
+coachApp.controller("NavCtrl", function ($scope, activeUser,$uibModal) { 
 
     $scope.isLoggedIn = function() {
         return activeUser.isLoggedIn();
     };
+
+ $scope.login = function() {
+        $uibModal.open({
+            templateUrl: "app/components/login/login.html",
+            controller: "LoginCtrl"
+        })
+    }
+
 });
