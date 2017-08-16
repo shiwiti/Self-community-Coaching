4d2c76829ebf528f53ad05209a68d83a1cf4a610
Lib Xp <libxp@stud-hait.ac.il>
2017-08-16 17:15:13 +0300

    בס"ד. הכנה להוספת רכיב carousel בעמוד home.html והוספת אפשרות הקלקה על הציור המוצג ומעבר לדף פרוט

--- app/app.js
+++ app/app.js
@@ -33,35 +33,3 @@ coachApp.config(function ($routeProvider) {
             controller: "visionUpdtCtrl"
         })
 });
-
-// TODO: Delete this TESTINGGGGGGGGGG please update
-// coachApp.controller("TestCtrl", function ($scope, User, activeUser, Recipe, recipes) {
-//     var plainUser = {
-//         "email": "Aharoni@Aharoni.com",
-//         "password": "a123",
-//         "firstName": "Aharoni",
-//         "lastName": "Levi",
-//         // "data": "Aharoni-data.json" יש 2 קבצי מידע: מטרות וחזון. איך נצרף ?
-//         "targets":
-//         "visions":
-//     }
-
-    // var user = new User(plainUser);
-
-    // console.log(JSON.stringify(user));
-    // console.log(activeUser.isLoggedIn());
-    // activeUser.login(user);
-    // console.log(JSON.stringify(activeUser.get()));
-    // console.log(activeUser.isLoggedIn());
-    // activeUser.logout();
-    // console.log(activeUser.isLoggedIn());
-
-
-    // var plainRecipe = {
-    //     "name": "Shakshuka",
-    //     "description": "An amazing egg dish",
-    //     "duration": 40,
-    //     "ingrediants": "Eggs, Tomattos, Oil",
-    //     "steps": "Heat the pan, put the eggs",
-    //     "imageUrl": "http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/c0.37.1080.1080/17438558_192469144582319_1152478392830918656_n.jpg?ig_cache_key=MTQ3NTg0MDMxNjE4ODA0ODY4NA%3D%3D.2.c"
-    // }
