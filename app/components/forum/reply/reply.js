09d83ad83cd47ae01b4dedc6d5178a6e53398346
Lib Xp <libxp@stud-hait.ac.il>
2017-08-24 18:02:32 +0300

    בס"ד. הוספת צמד עבור הוספת תגובה לדיון מסויים. יש להוסיף עוד צמד עבור viewDiscussion

--- app/components/forum/reply/reply.js
+++ app/components/forum/reply/reply.js
@@ -1,11 +1,26 @@
-
-
-
-            if($scope.discussion.reply.content){      // verify reply is not empty                  
-                discussions.addReply($routeParams.discussionsIndex, $scope.discussion.reply);
-                $location.path("/forum");
-            }
-            else {
-                alert("נא ודא תוכן בשדה תגובה" )
+coachApp.controller("newReplyCtrl", function ($scope, $location, Discussoin, discussoins, activeUser, $uibModalInstance) {
+    
+        // If the user is not logged in going back to home screen
+        if (!activeUser.isLoggedIn()) {
+            $location.path("/");
+            return;
+        }
+    
+    
+        $scope.cancel = function () {
+            $uibModalInstance.close("canceled");
+            $location.path("/forum");
+        }
+    
+        $scope.create = function () {
+            if($scope.discussion.replyIndex.content){      // verify reply is not empty     
+            $uibModalInstance.close("added");
+            discussions.addReply($routeParams.discussionsIndex, $scope.discussion.reply);
+            $location.path("/forum");
+        }
+        else {
+            alert("נא ודא תוכן בשדה תגובה" )
+        }
             }
     
+    });
